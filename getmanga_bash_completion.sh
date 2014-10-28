bae5e15685f02029ea719258d90b884cdde00458
Edgar S?nchez <esanchez@linux.cl>
2014-10-28 15:03:11 -0300

    Agregando archivo de configuracion para el autocompletado

--- getmanga_bash_completion.sh
+++ getmanga_bash_completion.sh
@@ -3,16 +3,25 @@ have getmanga &&
 _getmanga()
 {
     local cur
+    local pathFile="/home/esanchez/lenguajes/python/wget/list_manga.cfg"
+    local cmdLine
 
+    while read line
+    do
+     cmdLine="$cmdLine $line"
+    done < $pathFile
+
+    cmdLine="$cmdLine $line"
     COMPREPLY=()
     _get_comp_words_by_ref cur
 
     if [[ "$cur" == -* ]] ; then
-        COMPREPLY=( $( compgen -W '--help -h --info -U -C -L -V' -- "$cur" ) )
+        COMPREPLY=( $( compgen -W "--help -h --info -U -C -L -V" -- "$cur" ) )
     else
         if [ $COMP_CWORD -eq 1 ]; then
-            COMPREPLY=( $( compgen -W 'ant btooom claymore liar_game i_am_a_hero zetman \
-                                       dorohedoro slam-dunk sun_ken_rock' -- "$cur" ) )
+            #COMPREPLY=( $( compgen -W 'ant btooom claymore liar_game i_am_a_hero zetman \
+            #                           dorohedoro slam-dunk sun_ken_rock' -- "$cur" ) )
+            COMPREPLY=( $( compgen -W "$cmdLine" -- "$cur" ) )
         else
             _filedir rar
         fi
